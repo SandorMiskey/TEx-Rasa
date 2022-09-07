5e58d29dc9620aecdcf322daa9c91a0e021002df
Sandor Miskey <s@ndor.miskey.net>
2022-09-07 16:22:54 +0200

    mgmt static build

--- mgmt/build.sh
+++ mgmt/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-env GOOS=linux go build -ldflags="-extldflags=-static" -tags sqlite_omit_load_extension main.go
+env GOOS=linux go build -ldflags="-extldflags=-static" -tags sqlite_omit_load_extension,netgo main.go
 mv main storage/mgmt/
