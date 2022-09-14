c7cb11938c4f6c8ea1072399e0304006da2a8a51
Sandor Miskey <s@ndor.miskey.net>
2022-09-14 17:19:23 +0200

    cgo for static build

--- mgmt/build.sh
+++ mgmt/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-env GOOS=linux go build -ldflags="-extldflags=-static" -tags sqlite_omit_load_extension,netgo main.go
+env GOOS=linux CGO_ENABLED=1 go build -ldflags="-extldflags=-static" -tags sqlite_omit_load_extension,netgo main.go
 mv main storage/mgmt/
